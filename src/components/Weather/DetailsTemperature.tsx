1c232faf9b696bcac21912c58aaa663791c6a8c4
Artem Levchenko <levchencode@gmail.com>
2025-09-20 01:15:37 +0300

    temperature block

---
+++ src/components/Weather/DetailsTemperature.tsx
@@ -0,0 +1,23 @@
+import { WeatherApiCurrent } from "@/types/weather-api";
+
+export default function DetailsTemperature({
+    temp,
+    feelslike,
+}: {
+    temp: WeatherApiCurrent["temp_c"];
+    feelslike: WeatherApiCurrent["feelslike_c"];
+}) {
+    return (
+        <section className="flex items-center justify-evenly py-2">
+            <h3 className="sr-only">Temperature</h3>
+            <section className="flex flex-col">
+                <h4 className="text-muted-foreground text-xs">Now</h4>
+                <span className="text-lg font-bold">{temp} °C</span>
+            </section>
+            <section className="flex flex-col">
+                <h4 className="text-muted-foreground text-xs">Feels Like</h4>
+                <span className="text-lg font-bold">{feelslike} °C</span>
+            </section>
+        </section>
+    );
+}
