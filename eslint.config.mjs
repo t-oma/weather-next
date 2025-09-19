352fd1a12465016b6f5082a9941eb038e31a39d0
Artem Levchenko <levchencode@gmail.com>
2025-09-19 15:45:47 +0300

    shadcn & configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "prettier",
+        "plugin:jsx-a11y/recommended"
+    ),
+    ...compat.config({
+        rules: {
+            "jsx-a11y/alt-text": "warn",
+            "jsx-a11y/aria-props": "warn",
+            "jsx-a11y/aria-proptypes": "warn",
+            "jsx-a11y/aria-unsupported-elements": "warn",
+            "jsx-a11y/role-has-required-aria-props": "warn",
+            "jsx-a11y/role-supports-aria-props": "warn",
+        },
+    }),
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+    },
 ];
 
 export default eslintConfig;
