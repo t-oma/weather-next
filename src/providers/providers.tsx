c4f522d81d8214d1ced86c55aa4fd6d6e6361a9a
Artem Levchenko <levchencode@gmail.com>
2025-09-20 17:04:38 +0300

    all providers

---
+++ src/providers/providers.tsx
@@ -0,0 +1,15 @@
+import { QueryProvider } from "./query";
+import WeatherStoreProvider from "./weather";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    // NOTE: Avoid useState when initializing the query client if you don't
+    //       have a suspense boundary between this and the code that may
+    //       suspend because React will throw away the client on the initial
+    //       render if it suspends and there is no boundary
+
+    return (
+        <QueryProvider>
+            <WeatherStoreProvider>{children}</WeatherStoreProvider>
+        </QueryProvider>
+    );
+}
