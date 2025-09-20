a50a847b032a2e7f02e1001f6995faf872208b23
Artem Levchenko <levchencode@gmail.com>
2025-09-20 15:39:20 +0300

    api request takes lat,lng and options

--- src/lib/api/weather.ts
+++ src/lib/api/weather.ts
@@ -1,30 +1,34 @@
 import { testData, WeatherApiResponse } from "@/types/weather-api";
 import { hget } from "./helpers";
+import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
+/**
+ * Get weather data for a given location
+ * @param location Location name
+ * @throws Error if API key is not set or location is invalid
+ * @returns Weather data
+ */
 export async function getWeather(
-    location: string
-): Promise<WeatherApiResponse | null> {
-    const API_KEY = process.env.WEATHER_API_KEY;
+    latitude: number,
+    longitude: number,
+    options?: RequestInit
+): Promise<WeatherApiResponse> {
+    const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
     if (!API_KEY) {
-        console.error("Missing WEATHER_API_KEY environment variable");
-        return null;
+        throw new Error("Missing WEATHER_API_KEY environment variable");
     }
 
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=1&aqi=no&alerts=no`;
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`;
 
     return testData;
 
-    try {
-        const response = await hget<WeatherApiResponse>(url, {
-            next: {
-                revalidate: 60,
-                tags: ["weather", `location:${location}`],
-            },
-        });
+    const response = await hget<WeatherApiResponse>(url, {
+        next: {
+            revalidate: 60,
+            tags: ["weather", `location:${latitude},${longitude}`],
+        },
+        ...options,
+    });
 
-        return response;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
+    return response;
 }
