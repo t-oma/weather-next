2e1d3d403ef8707bd24b3ac56596432958eee599
Artem Levchenko <levchencode@gmail.com>
2025-09-20 15:39:39 +0300

    orientation

--- src/components/Weather/DetailsItem.tsx
+++ src/components/Weather/DetailsItem.tsx
@@ -1,23 +1,30 @@
 import { ClassNameValue } from "tailwind-merge";
 import DetailsIconWrapper from "./DetailsIconWrapper";
+import { cn } from "@/lib/utils";
 
 export default function DetailsItem({
     title,
     icon,
     iconBg,
     titled = false,
+    orientation = "horizontal",
     children,
 }: Readonly<{
     title: string;
     icon: React.ReactNode;
     iconBg: ClassNameValue;
     titled?: boolean;
+    orientation?: "horizontal" | "vertical";
     children: React.ReactNode;
 }>) {
     return (
         <section
             aria-label={!titled ? title : undefined}
-            className="flex items-center gap-2"
+            className={cn(
+                "flex gap-2",
+                orientation === "vertical" && "flex-col items-start",
+                orientation === "horizontal" && "flex-row items-center"
+            )}
         >
             <DetailsIconWrapper className={iconBg}>{icon}</DetailsIconWrapper>
             {titled ? (
