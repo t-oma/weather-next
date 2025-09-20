6a21858121c78e4c6fcc68b6490f81aee7c95d19
Artem Levchenko <levchencode@gmail.com>
2025-09-20 15:40:01 +0300

    weather provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WeatherStoreProvider from "@/providers/weather";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className="antialiased">
-                <div className="flex min-h-screen flex-col">{children}</div>
+                <div className="flex min-h-screen flex-col">
+                    <WeatherStoreProvider>{children}</WeatherStoreProvider>
+                </div>
             </body>
         </html>
     );
