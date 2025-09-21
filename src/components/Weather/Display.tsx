0ec569c525e74ce4ea95e6d8d14452b7034d087f
Artem Levchenko <levchencode@gmail.com>
2025-09-21 14:22:41 +0300

    styles improvements

--- src/components/Weather/Display.tsx
+++ src/components/Weather/Display.tsx
@@ -5,9 +5,9 @@ import View from "./View";
 
 export default function WeatherDisplay() {
     return (
-        <div className="bg-background container flex h-[70vh] overflow-hidden rounded-lg">
+        <div className="container flex h-[70vh] overflow-hidden rounded-lg">
             <View />
-            <div className="flex flex-1/3 flex-col">
+            <div className="bg-background flex flex-1/3 flex-col">
                 <div className="text-background flex flex-col items-center justify-center bg-black p-4">
                     <p className="font-semibold">Weather Next</p>
                     <p className="text-muted text-xs">by Artem Levchenko</p>
