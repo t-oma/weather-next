8d09500da8a8497ef4012d69e5996d206ae38988
Artem Levchenko <levchencode@gmail.com>
2025-09-21 12:13:06 +0300

    normalize coords

--- src/hooks/useWeatherQuery.ts
+++ src/hooks/useWeatherQuery.ts
@@ -1,6 +1,7 @@
 import { QueryKey, useQuery, UseQueryResult } from "@tanstack/react-query";
 import { getWeather } from "@/lib/api/weather";
 import { WeatherApiResponse } from "@/types/weather-api";
+import { normalizeCoords } from "@/lib/geo";
 
 type Coords = { lat: number; lon: number };
 
@@ -8,11 +9,13 @@ export function useWeatherQuery<TSelected = WeatherApiResponse>(
     coords: Coords | null,
     select?: (data: WeatherApiResponse) => TSelected
 ): UseQueryResult<TSelected> {
+    const nc = coords ? normalizeCoords(coords, 6) : null;
+
     return useQuery<WeatherApiResponse, Error, TSelected, QueryKey>({
-        queryKey: ["weather", `${coords?.lat},${coords?.lon}`],
-        queryFn: ({ signal }) =>
-            getWeather(coords!.lat, coords!.lon, { signal }),
-        enabled: !!coords,
+        queryKey: ["weather", `${nc?.lat},${nc?.lon}`],
+        queryFn: async ({ signal }) =>
+            await getWeather(nc!.lat, nc!.lon, { signal }),
+        enabled: !!nc,
         staleTime: 5 * 60 * 1000,
         refetchOnWindowFocus: true,
         select,
