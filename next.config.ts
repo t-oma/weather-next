724897b7542925f198ac36c907287b7673e0c3de
Artem Levchenko <levchencode@gmail.com>
2025-09-21 14:22:50 +0300

    remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "places.googleapis.com",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
