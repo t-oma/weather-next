9772f333ec96878f5c7272365ba57c47e28d0466
Artem Levchenko <levchencode@gmail.com>
2025-09-21 14:22:29 +0300

    image added

--- src/components/Weather/View.tsx
+++ src/components/Weather/View.tsx
@@ -1,10 +1,12 @@
 import SettingsMenu from "./SettingsMenu";
+import ViewImage from "./ViewImage";
 import ViewInfo from "./ViewInfo";
 
 export default function View() {
     return (
-        <div className="bg-primary relative flex-2/3">
+        <div className="relative flex-2/3">
             <SettingsMenu />
+            <ViewImage />
             <ViewInfo />
         </div>
     );
